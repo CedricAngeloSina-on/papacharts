1ff860645b7a99abca3e24794afc14123b146af9
CedricAngeloSina-on <cao.sinaon@gmail.com>
2024-08-12 23:11:06 +0800

    feat: csv-form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { CSVForm } from "@/components/custom/csv-form";
+
 export default function HomePage() {
   return (
     <main className="flex min-h-[calc(100vh-4rem)] flex-col items-center justify-center lg:flex-row">
       <div className="flex w-1/2 flex-col items-center">
-        <p className="text-4xl font-bold text-primary">RENDER FORM HERE</p>
+        <CSVForm />
       </div>
       <div className="flex w-1/2 flex-col items-center">
         <p className="text-4xl font-bold text-primary">RENDER CHART HERE</p>
