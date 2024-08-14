ab3aaefe0063b2a9257efe3854589df94b061add
CedricAngeloSina-on <cao.sinaon@gmail.com>
2024-08-14 20:29:41 +0800

    chore: render components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
-import { CSVForm } from "@/components/custom/csv-form";
+import CSVForm from "@/components/custom/csv-form";
+import Barchart from "@/components/custom/barchart";
 
 export default function HomePage() {
   return (
     <main className="flex min-h-[calc(100vh-4rem)] flex-col items-center justify-center lg:flex-row">
-      <div className="flex w-1/2 flex-col items-center">
+      <div className="flex w-1/2 flex-col items-center px-4">
         <CSVForm />
       </div>
-      <div className="flex w-1/2 flex-col items-center">
-        <p className="text-4xl font-bold text-primary">RENDER CHART HERE</p>
+      <div className="flex w-1/2 flex-col items-center px-4">
+        <Barchart />
       </div>
     </main>
   );
